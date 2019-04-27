d7e96d57f0803e7829482d709a4e984f873265a1
Denis Mitryakov <denisloverap@icloud.com>
2019-04-28 02:08:04 +0700

    Update portfolio site

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
-# Резюме
-
-<p>Здесь хранятся законченные работы, выкладываю их одним коммитом, этапы разработки можно отследить в репозиториях c названиями: <em>profile-project-$</em></p>
-
-Ссылка на проект, ссылка на макет.
-<ol>
-  <li><strong><a href="https://denisonepiece.github.io/app/index.html">Portfolio(beta-verison)</a></strong>, По примеру</li>
-  <li><strong><a href="https://denisonepiece.github.io/lian/index.html">Lian</a></strong>, <a href="https://freebies.fluxes.com/lian-free-portfolio-template/">PSD</a></li>
-  <li><strong><a href="https://denisonepiece.github.io/test-work/index.html">Tajam(test work)</a></strong>, soon</li>
-</ol>
+# profile-project-2
--- app/js/cube.js
+++ app/js/cube.js
@@ -2,7 +2,7 @@ var canvas = document.getElementById('cube');
 var ctx = canvas.getContext('2d');
 
 
-ctx.scale(2, 2);
+ctx.scale(1, 1);
 
 ctx.beginPath();
 ctx.moveTo(0, 100);
@@ -31,32 +31,38 @@ ctx.lineTo(0, 100);
 ctx.moveTo(200, 100);
 ctx.lineTo(100, 50);
 
-var color = [
-    '#791CEF', 
-    '#6220EF', 
-    '#4A25EF', 
-    '#2E2BEF', 
-    '#254CEE', 
-    '#1F6BED', 
-    '#198CEC', 
-    '#11BBEB', 
-    '#198CEC', 
-    '#1F6BED', 
-    '#254CEE', 
-    '#2E2BEF',
-    '#4A25EF',
-    '#6220EF', 
-];
+// var color = [
+//     '#791CEF',
+//     '#6220EF',
+//     '#4A25EF',
+//     '#2E2BEF',
+//     '#254CEE',
+//     '#1F6BED',
+//     '#198CEC',
+//     '#11BBEB',
+//     '#198CEC',
+//     '#1F6BED',
+//     '#254CEE',
+//     '#2E2BEF',
+//     '#4A25EF',
+//     '#6220EF',
+// ];
 
 var i = 0;
 
 setInterval(function() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.strokeStyle = color[i];
+    ctx.strokeStyle = randColor();
     ctx.stroke();
-    i++;
-    if(i == 14) {
-        i = 0;
-    }
-}, 200);
+    // i++;
+    // if(i == 14) {
+    //     i = 0;
+    // }
+}, 300);
 
+function randColor() {
+    var r = Math.floor(Math.random() * (256)),
+        g = Math.floor(Math.random() * (256)),
+        b = Math.floor(Math.random() * (256));
+    return '#' + r.toString(16) + g.toString(16) + b.toString(16);
+}
