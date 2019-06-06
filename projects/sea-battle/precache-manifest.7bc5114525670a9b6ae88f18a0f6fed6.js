487878b3e84868ed87b409bd1a52b6c6b3867692
denisonepiece <denisloverap@icloud.com>
2019-06-07 06:41:45 +0700

    Add new project - sea-battle

---
+++ projects/sea-battle/precache-manifest.7bc5114525670a9b6ae88f18a0f6fed6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0df74fd97e5c1fc064639a927081ecf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37714cd0f72842ec4b93",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "bf9edaf94ee50508bb0d",
+    "url": "/static/css/main.33d8e3e0.chunk.css"
+  },
+  {
+    "revision": "37714cd0f72842ec4b93",
+    "url": "/static/js/2.5d85cbe7.chunk.js"
+  },
+  {
+    "revision": "bf9edaf94ee50508bb0d",
+    "url": "/static/js/main.b950ba43.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
