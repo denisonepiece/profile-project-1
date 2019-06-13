be5d3fb693b14ab30e217140d457c1eb9bb6c684
denisonepiece <denisloverap@icloud.com>
2019-06-14 05:41:46 +0700

    Eslint and stab

--- app/js/main.js
+++ app/js/main.js
@@ -1,76 +1,94 @@
-window.addEventListener('DOMContentLoaded', function () {
+'use sctrict';
 
-  // animationHeaderLogo();
-  animationNickName();
+const animationHeaderLogo = function() {
+  const logoBlock = document.querySelector('.header__logo');
+  window.addEventListener('scroll', () => {
+    logoBlock.style.transform = 'rotate(' + window.pageYOffset / 2 + 'deg)';
+  });
+};
 
-  function animationHeaderLogo() {
-    let logoBlock = document.querySelector('.header__logo'),
-      logo = logoBlock.querySelector('img');
+const animationNickName = function() {
+  const preview = document.querySelector('.preview');
+  const nickNumbers = document.querySelector('.nick-numbers');
+  const pizza = document.querySelector('.pizza');
+  const pieces = pizza.querySelectorAll('img');
+  let top = -345;
 
-    window.addEventListener('scroll', function () {
-      logoBlock.style.transform = 'rotate(' + window.pageYOffset / 2 + 'deg)';
-    });
-  };
-
-  function animationNickName() {
-    let nickNumbers = document.querySelector('.nick-numbers'),
-      top = -345,
-      up = true,
-      rotate = 0,
-      height = 200,
-      pizza = document.querySelector('.pizza'),
-      pieces = pizza.querySelectorAll('img'),
-      emptyDiv = document.createElement('div');
-
-
-    let stopId = setInterval(function () {
-      if (top == -345) {
-        up = false;
-      }
-
-      if (top == 0) {
-        up = true;
-        pieces[0].style.position = 'unset';
-      }
-
-      if (rotate == -264) {
-        clearInterval(stopId);
-      }
-
-      switch (top) {
-        case -345:
-          pieces[5].style.opacity = 0;
-          break;
-        case -274:
-          pieces[4].style.opacity = 0;
-          break;
-        case -205:
-          pieces[3].style.opacity = 0;
-          break;
-        case -137:
-          pieces[2].style.opacity = 0;
-          break;
-        case -69:
-          pieces[1].style.opacity = 0;
-          break;
-        case 0:
-          pieces[0].style.opacity = 1;
-          break;
-      }
-
-      switch (up) {
-        case true:
-          rotate -= 6;
-          height += 1;
-          break;
-        case false:
-          top++;
-          break;
-      }
-
+  setInterval(() => {
+    if (top <= 0) {
       nickNumbers.style.top = top + 'px';
-    }, 10);
-  };
+    }
 
+    if (top === 30) {
+      preview.style.display = 'none';
+    }
 
-});
+    switch (top) {
+      case -345:
+        pieces[5].style.opacity = 0.3;
+        break;
+      case -274:
+        pieces[4].style.opacity = 0.3;
+        break;
+      case -205:
+        pieces[3].style.opacity = 0.3;
+        break;
+      case -137:
+        pieces[2].style.opacity = 0.3;
+        break;
+      case -69:
+        pieces[1].style.opacity = 0.3;
+        break;
+      case 0:
+        pieces[0].style.opacity = 1;
+        break;
+    }
+
+    top++;
+  }, 7);
+};
+
+const flicker = function() {
+  const body = document.body;
+  let sec = 0;
+  let invert = false;
+
+  setInterval(() => {
+    if (sec === 80) {
+      invert = !invert;
+    }
+
+    if (sec === 82) {
+      invert = !invert;
+    }
+
+    if (sec === 92) {
+      invert = !invert;
+    }
+
+    if (sec === 95) {
+      invert = !invert;
+    }
+
+    if (sec === 98) {
+      invert = !invert;
+    }
+
+    if (sec === 100) {
+      sec = 0;
+      invert = !invert;
+    }
+
+    if (invert) {
+      body.style.filter = 'invert(1)';
+    } else {
+      body.style.filter = 'invert(0)';
+    }
+
+    sec += 1;
+  }, 100);
+};
+
+animationHeaderLogo();
+animationNickName();
+// flicker();
